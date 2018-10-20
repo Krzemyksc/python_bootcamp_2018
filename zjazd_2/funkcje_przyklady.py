ae8ce96aa825d1c8727d79e488aca4043db485e7
Krzemyksc <krzem96@gmail.com>
2018-10-20 16:25:08 +0200

    zadania cz.3

---
+++ zjazd_2/funkcje_przyklady.py
@@ -0,0 +1,39 @@
+# # def przywitaj_się():
+# #     return 1
+# #
+# #
+# # print(przywitaj_się())
+#
+#
+# def przywitaj_sie(imie):
+#     print("Witaj", imie)
+#
+# lista_imion = ['Rafał', 'Adam', 'Agnieszka']
+#
+# for imie in lista_imion:
+#     przywitaj_sie(imie)
+
+# def przywitaj_sie(imie,wiek,waga,wzrost):
+#     print("Witaj", imie)
+
+
+ lista_osob = [
+    {'imie': "Mateusz",
+    'wiek': 38,
+     'wzrost': 178,
+     'waga' : 102,
+    },
+    {'imie': "Adam",
+    'wiek': 40,
+     'wzrost': 178,
+     'waga' : 80,
+    },
+    {'imie': "ada",
+    'wiek': 38,
+     'wzrost': 185,
+     'waga' : 70,
+    }
+]
+for osoba in lista_osob:
+    przywitaj_sie(osoba['imie'],osoba)
+
