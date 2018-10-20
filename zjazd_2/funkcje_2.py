a12aff20e55eb370a735abffa24cf61a14625298
Krzemyksc <krzem96@gmail.com>
2018-10-20 16:46:26 +0200

    zadania cz 4

--- zjazd_2/funkcje_2.py
+++ zjazd_2/funkcje_2.py
@@ -1,13 +1,22 @@
+# def wiecej_niz(napis, prog):
+#     liczniki_liter = {}
+#     wynik = set()
+#     for litera in napis:
+#         liczniki_liter[litera] = liczniki_liter.get(litera, 0) + 1
+#
+#     for key, value in liczniki_liter.items():
+#         if value > prog:
+#             wynik.add(key)
+#     return wynik
+
 def wiecej_niz(napis, prog):
-    liczniki_liter = {}
     wynik = set()
     for litera in napis:
-        liczniki_liter[litera] = liczniki_liter.get(litera, 0) + 1
-
-    for key, value in liczniki_liter.items():
-        if value > prog:
-            wynik.add(key)
+        litera.lower()
+        if napis.count(litera)>prog:
+            wynik.add(litera)
     return wynik
+
 
 
 
