b9682cd0bc2995ec6906c35f9e26b7ea63e46857
Krzemyksc <krzem96@gmail.com>
2018-12-01 16:50:52 +0100

    zjazd5

---
+++ zjazd_5/przykłady.py
@@ -0,0 +1,4 @@
+### wprzypadku cwiczenia_numpy wszystkie obiekty muszą być jednego typu
+#jupyter cwiczenia_numpy jak dam reshape i tam są dwie osie x jako o i idzie w poziom i y jako 1 i idzie w dół
+#Kazda z wersó cwiczenia_pandas dizeki komendzie series jest serią posiadającą index i wartośći
+###
